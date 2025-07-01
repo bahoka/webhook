7efc8103c266a0ac8b0723a96617d868b2e274c0
bahoka <bahokas@gmail.com>
2025-07-01 15:03:18 +0200

    test2

--- db.py
+++ db.py
@@ -5,32 +5,31 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# создаем одно подключение на всё приложение
+# создаем подключение
 conn = psycopg2.connect(DATABASE_URL)
 conn.autocommit = True
 
-# инициализация базы
 def init_db():
     with conn.cursor() as cur:
-        cur.execute("""
+        cur.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
                 phone TEXT UNIQUE NOT NULL,
                 chat_id BIGINT NOT NULL
             )
-        """)
+        ''')
 
 def save_user(phone, chat_id):
     with conn.cursor() as cur:
-        cur.execute("""
+        cur.execute('''
             INSERT INTO users (phone, chat_id)
             VALUES (%s, %s)
             ON CONFLICT (phone)
             DO UPDATE SET chat_id = EXCLUDED.chat_id
-        """, (phone, chat_id))
+        ''', (phone, chat_id))
 
 def get_chat_id_by_phone(phone):
     with conn.cursor() as cur:
-        cur.execute("SELECT chat_id FROM users WHERE phone = %s", (phone,))
+        cur.execute('SELECT chat_id FROM users WHERE phone = %s', (phone,))
         result = cur.fetchone()
         return result[0] if result else None
